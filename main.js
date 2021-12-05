ab6aa9a05cd4c4086b8ca93cc2759c08aed0763b
MohAlkhalaileh <kalaileh.mohammad01@gmail.com>
2021-12-05 14:24:26 +0200

    5

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-// const quizContainer = document.getElementById('quiz');
-//     const resultsContainer = document.getElementById('results');
-//     const submitButton = document.getElementById('submit');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
 
-// const myQuestions = [
-//     {Image:  src="./img/q1.png",
-//     question:"1.Answer choices for the first question." ,
-//     answers: {
-//         a: "john",
-//         b: "jane",
-//         c: "none"
-//     },
-//     correctAnswer: "a"
-//     },
-//     {
-//     Image: src="img/q2.png",
-//     question: "2.Answer choices for the second question.",
-//     answers: {
-//         a: "14",
-//         b: "10",
-//         c: "15"
-//     },
-//     correctAnswer: "b"
-//     },
-//     {
-//     Image: src="img/q3.png",
-//     question: "3.Answer choices for the third question.",
-//     answers: {
-//         a: "26",
-//         b: "24",
-//         c: "10",
-//     },
-//     correctAnswer: "c"
-//     }
-// ];
-(function(){
+const myQuestions = [
+    {
+    question:"1.Answer choices for the first question." ,
+    answers: {
+        a: "john",
+        b: "jane",
+        c: "none"
+    },
+    correctAnswer: "a"
+    },
+    {
+    
+    question: "2.Answer choices for the second question.",
+    answers: {
+        a: "14",
+        b: "10",
+        c: "15"
+    },
+    correctAnswer: "b"
+    },
+    {
+    question: "3.Answer choices for the third question.",
+    answers: {
+        a: "26",
+        b: "24",
+        c: "10",
+    },
+    correctAnswer: "c"
+    }
+];
+
     function buildQuiz(){
     const output = [];
     myQuestions.forEach(
@@ -64,41 +63,25 @@
     const answerContainers = quizContainer.querySelectorAll('.answers');
     let numCorrect = 0;
 
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+        
+        answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
-
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-    {
-    question:"1.Answer choices for the first question." ,
-    answers: {
-        a: "john",
-        b: "jane",
-        c: "none"
-    },
-    correctAnswer: "a"
-    },
-    {
-    question: "2.Answer choices for the second question.",
-    answers: {
-        a: "14",
-        b: "10",
-        c: "15"
-    },
-    correctAnswer: "b"
-    },
-    {
-    question: "3.Answer choices for the third question.",
-    answers: {
-        a: "26",
-        b: "24",
-        c: "10",
-    },
-    correctAnswer: "c"
-    }
-];
+    
     buildQuiz();
     submitButton.addEventListener('click', showResults);
-})();
